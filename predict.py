778509f0f50c554f2e1fd500f321b4eb97968b32
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-04-13 17:27:38 -0700

    Update dependencies in cog.yaml and fix config path in predict.py

--- predict.py
+++ predict.py
@@ -1,5 +1,4 @@
 from cog import BasePredictor, BaseModel, Path
-import trimesh
 from omegaconf import OmegaConf
 from samesh.data.loaders import *
 from samesh.models.sam_mesh import *
@@ -8,6 +7,7 @@ from samesh.models.sam_mesh import *
 class ModelOutput(BaseModel):
     segmented_mesh: Path
 
+
 class Predictor(BasePredictor):
     def setup(self):
         pass
@@ -15,14 +15,14 @@ class Predictor(BasePredictor):
     def predict(self, mesh_file: Path) -> ModelOutput:
         """
         Run SAMesh segmentation on the input mesh.
-        
+
         Args:
             mesh_file (Path): Path to the input mesh file in GLB format.
-        
+
         Returns:
             ModelOutput: Contains the path to the segmented mesh in GLB format.
         """
-        config = OmegaConf.load('../configs/mesh_segmentation.yaml')
+        config = OmegaConf.load("thirdparty/samesh/configs/mesh_segmentation.yaml")
         segmented_mesh = segment_mesh(mesh_file, config, visualize=False)
         output_path = self.output_path("segmented_mesh.glb")
         segmented_mesh.export(output_path)
